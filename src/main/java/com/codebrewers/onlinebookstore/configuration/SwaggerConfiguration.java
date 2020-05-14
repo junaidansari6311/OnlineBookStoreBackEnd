a0480ff8994c18078197c7f44a099bff74a668c1
Pritamsuryawanshi <pritam.surya110@gmail.com>
2020-05-14 23:35:14 +0530

    MODIFIED : Configuration's class name

--- src/main/java/com/codebrewers/onlinebookstore/configuration/SwaggerConfiguration.java
+++ src/main/java/com/codebrewers/onlinebookstore/configuration/SwaggerConfiguration.java
@@ -10,8 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class AppConfiguration {
-
+public class SwaggerConfiguration {
 
     @Bean
     public Docket api() {
