c1d20d1c6665a5141d47824a4c8b9b80f9032f04
gajanan15 <gajanandesai97@gmail.com>
2020-05-18 12:26:21 +0530

    ADDED : Functionality for add book to the shopping cart

---
+++ src/main/java/com/codebrewers/onlinebookstore/exception/CartException.java
@@ -0,0 +1,7 @@
+package com.codebrewers.onlinebookstore.exception;
+
+public class CartException extends RuntimeException {
+    public CartException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/codebrewers/onlinebookstore/service/implementation/CartService.java
+++ src/main/java/com/codebrewers/onlinebookstore/service/implementation/CartService.java
@@ -1,10 +1,14 @@
 package com.codebrewers.onlinebookstore.service.implementation;
 
 import com.codebrewers.onlinebookstore.dto.CartDTO;
+import com.codebrewers.onlinebookstore.exception.CartException;
+import com.codebrewers.onlinebookstore.model.CartDetails;
 import com.codebrewers.onlinebookstore.repository.IcartRepository;
 import com.codebrewers.onlinebookstore.service.ICartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CartService implements ICartService {
@@ -14,6 +18,12 @@ public class CartService implements ICartService {
 
     @Override
     public String addTOCart(CartDTO cartDTO) {
-        return null;
+        CartDetails cartDetails = new CartDetails(cartDTO);
+        Optional<CartDetails> byBookName = icartRepository.findByBookName(cartDTO.bookName);
+        if(byBookName.isPresent()){
+            throw new CartException("Book Already Present");
+        }
+        icartRepository.save(cartDetails);
+        return "book addded";
     }
 }
