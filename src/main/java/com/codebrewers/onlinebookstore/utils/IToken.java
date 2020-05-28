c066478b7d90335b71d9b5ed9b2ce209a14a67dc
junaidansari6311 <junaidansari6311@gmail.com>
2020-05-28 16:06:06 +0530

    ADDED : Functionality to decode JWT token

--- src/main/java/com/codebrewers/onlinebookstore/utils/IToken.java
+++ src/main/java/com/codebrewers/onlinebookstore/utils/IToken.java
@@ -1,9 +1,12 @@
 package com.codebrewers.onlinebookstore.utils;
 
+import com.codebrewers.onlinebookstore.exception.JWTException;
 import com.codebrewers.onlinebookstore.model.UserDetails;
 
 public interface IToken {
 
      String generateLoginToken(UserDetails userDetails);
+
+     int decodeJWT(String jwt) throws JWTException;
 
 }
--- src/main/java/com/codebrewers/onlinebookstore/utils/implementation/Token.java
+++ src/main/java/com/codebrewers/onlinebookstore/utils/implementation/Token.java
@@ -1,9 +1,12 @@
 package com.codebrewers.onlinebookstore.utils.implementation;
 
 
+import com.codebrewers.onlinebookstore.exception.JWTException;
 import com.codebrewers.onlinebookstore.model.UserDetails;
 import com.codebrewers.onlinebookstore.properties.FileProperties;
 import com.codebrewers.onlinebookstore.utils.IToken;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +31,18 @@ public class Token implements IToken {
                 .setExpiration(new Date(currentTime + jwtProperties.getJwtExpirationMs()))
                 .signWith(SignatureAlgorithm.HS256, jwtProperties.getJwtSecret())
                 .compact();
+    }
+
+    @Override
+    public int decodeJWT(String jwt) throws JWTException {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(jwtProperties.getJwtSecret()).parseClaimsJws(jwt).getBody();
+
+            return Integer.parseInt(claims.getId());
+        } catch (ExpiredJwtException e) {
+            throw new JWTException("session time out");
+        }
     }
 
 }
