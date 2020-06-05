e459e2a4931e7cefb467d5846d992cbf9c7a0cb7
junaidansari6311 <junaidansari6311@gmail.com>
2020-06-05 12:36:57 +0530

    ADDED : Relational mapping between order, customer, user, cart and  bookcart

---
+++ src/main/java/com/codebrewers/onlinebookstore/model/OrderDetails.java
@@ -0,0 +1,57 @@
+package com.codebrewers.onlinebookstore.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class OrderDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer id;
+
+    public Integer orderId;
+    public Double totalPrice;
+
+    public LocalDate orderPlacedDate;
+
+    @JsonIgnore
+    @ManyToOne()
+    @JoinColumn(name = "cartId")
+    public CartDetails cart;
+
+    @JsonIgnore
+    @ManyToOne()
+    @JoinColumn(name = "userId")
+    public UserDetails user;
+
+    @JsonIgnore
+    @ManyToOne()
+    @JoinColumn(name = "customer")
+    public CustomerDetails customer;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "orderDetails")
+    List<BookCartDetails> bookCartDetails;
+
+
+    public OrderDetails() {
+    }
+
+    public OrderDetails(CartDetails cart,Integer orderId, UserDetails user, Double totalPrice, CustomerDetails customerDetails,List<BookCartDetails> bookCartDetails) {
+        this.user = user;
+        this.cart = cart;
+        this.customer = customerDetails;
+        this.totalPrice = totalPrice;
+        this.orderPlacedDate = LocalDate.now();
+        this.bookCartDetails = bookCartDetails;
+        this.orderId = orderId;
+    }
+
+}
